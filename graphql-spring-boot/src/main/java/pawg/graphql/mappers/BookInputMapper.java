e2d3bcda34e678eb5e49a6ea170674370346e7dc
Precyzik <gawaprecyz@gmail.com>
2025-08-01 20:34:40 +0200

    Fixing mappers.

--- graphql-spring-boot/src/main/java/pawg/graphql/mappers/BookInputMapper.java
+++ graphql-spring-boot/src/main/java/pawg/graphql/mappers/BookInputMapper.java
@@ -1,15 +1,19 @@
 package pawg.graphql.mappers;
 
-import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import pawg.graphql.books.BookEntity;
 import pawg.graphql.books.BookInput;
+
+import java.util.List;
 
 @Mapper
 public interface BookInputMapper {
     BookInputMapper INSTANCE = Mappers.getMapper(BookInputMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     BookEntity bookInputToBookEntity(BookInput bookInput);
+
     List<BookEntity> bookInputsToBookEntities(List<BookInput> bookInputs);
 }
--- graphql-spring-boot/src/main/java/pawg/graphql/mappers/UserInputMapper.java
+++ graphql-spring-boot/src/main/java/pawg/graphql/mappers/UserInputMapper.java
@@ -1,15 +1,19 @@
 package pawg.graphql.mappers;
 
-import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import pawg.graphql.users.UserEntity;
 import pawg.graphql.users.UserInput;
+
+import java.util.List;
 
 @Mapper
 public interface UserInputMapper {
     UserInputMapper INSTANCE = Mappers.getMapper(UserInputMapper.class);
 
+    @Mapping(target = "id", ignore = true)
     UserEntity userInputToUserEntity(UserInput userInput);
+
     List<UserEntity> userInputsToUserEntities(List<UserInput> userInputs);
 }
